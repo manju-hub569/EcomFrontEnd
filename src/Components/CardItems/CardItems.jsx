12298567559b96018b8dee85537e9980ebd63332
manjunath <manjunath.dhabe@dealmoney.in>
2024-01-02 15:18:29 +0530

    changes

--- src/Components/CardItems/CardItems.jsx
+++ src/Components/CardItems/CardItems.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './CardItems.css';
+import { CiHeart } from "react-icons/ci";
 
 const CardItems = ({items}) => {
     return (
         <>
-            <div className = "card m-2" style = {{width : '15rem'}}>
-            <img className = "card-img-top" src={items.image} alt="Card image cap" />
-                <div className = "card-body">
-                    <p className = "card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className = "cards">
+                <div className = "imgCard">
+                    <img src = {items.image} alt="image" />
+                </div>
+                <div className="subcard">
+                    <div className = "prodName">
+                        <div>{items.title}</div>
+                        <div>
+                            <CiHeart fontSize={'2rem'} />
+                        </div>
+                    </div>
+                    jdbksjdf
                 </div>
             </div>
         </>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,13 +2,14 @@ import './Home.css';
 import CardItems from '../CardItems/CardItems';
 import FootComp from '../FootComp/FootComp';
 import { useEffect , useState} from 'react';
+import { IoIosSearch } from "react-icons/io";
 
 export default function Home () {
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
         .then(res=>res.json())
-        .then(json=>setData(json))
+        .then(json =>setData(json));
     },[]);
 
     const [data, setData] = useState([]);
@@ -21,13 +22,23 @@ export default function Home () {
                             <h3 style = {{fontFamily: 'Montserrat'}}>PRODUCT OVERVIEW</h3>
                         </div>
                         <div className = "col-6 dflex alignCenter justifyEnd">
-                            <input type = "text" />
+                            <div className = "searchBox">
+                                <IoIosSearch fontSize={'30px'} />
+                                <input type = "text" placeholder='  Search Items' />
+                            </div>
                         </div>
                 </div>
             </div>
 
-            <div className = "container mt-3">
+            <div className = "container mt-1">
                 <div className = "row">
+                    {
+                        data.forEach((val) => {
+                            if(val.title.length > 65) {
+                                val.title = `BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats`
+                            }
+                        })
+                    }
                 {
                     data.map((val , i) => {
                         return (
