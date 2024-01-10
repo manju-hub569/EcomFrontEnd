d00c1b36c82b6b3d11dc3cb2d261e5abff7988f6
Manjunath Dhabe <67554477+manju-hub569@users.noreply.github.com>
2024-01-10 22:19:58 +0530

    logn-page

--- src/Container/Main/Modal.jsx
+++ src/Container/Main/Modal.jsx
@@ -1,4 +1,4 @@
-
+import './Main.css';
 
 export default function Modal () {
     return (
@@ -6,19 +6,28 @@ export default function Modal () {
             <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                 <div className="modal-dialog modal-dialog-centered" role="document">
                         <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            ...
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
+                            <div className="modal-body">
+
+                                <div className = "log_label">
+                                    <strong>Login</strong>
+                                </div>
+
+                                <div className = "inpts">
+                                    <div className = "inpt">
+                                        <input type = "text"  />
+                                    </div>
+                                    <div className = "inpt">
+                                        <input type = "password" />
+                                    </div>
+                                </div>
+
+                                <div className = "log-btn">
+                                    <button>
+                                        Logn In
+                                    </button>
+                                </div>
+
+                            </div>
                         </div>
                 </div>
             </div>
