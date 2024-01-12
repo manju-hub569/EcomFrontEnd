3e2a8fcceeb7a7ada2faf2c8d614275d94535270
manjunath <manjunath.dhabe@dealmoney.in>
2024-01-12 10:11:58 +0530

    changes

--- src/methods/apimethods.jsx
+++ src/methods/apimethods.jsx
@@ -2,14 +2,22 @@ import axios from 'axios';
 
 export const getCall = (url , header = {}) => {
     return new Promise(async (res, rej) => {
-        let data = await axios.get(url , header);
-        res(data);
+        try {
+            let data = await axios.get(url , header);
+            res(data);            
+        } catch (error) {
+            rej(error);
+        }
     })
 }
 
 export const postCall = (url , body , header = {}) => {
     return new Promise( async (res, rej) => {
-        let data = await axios.post(url , body , header);
-        res(data);
+        try {
+            let data = await axios.post(url , body , header);
+            res(data);            
+        } catch (error) {
+            rej(error);
+        }
     })
 }
